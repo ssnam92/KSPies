bbdae87be45cff10a533b06cc143afb0715280e4
Ryan J McCarty <43008464+RyanJMcCarty@users.noreply.github.com>
2021-03-10 16:13:05 -0800

    Manylinux updates to setup.py
    Added content to build many Linux wheel for version 1.0.0

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 import setuptools
+from numpy.distutils.core import setup, Extension
+
+print(setuptools.find_packages())
+
+lib = Extension(
+        name='kspies.kspies_fort',
+        sources=['kspies/kspies_fort.f90'],
+        library_dirs=['/usr/lib/'],
+        include_dirs=['/usr/include'],
+        libraries=['lapack']
+        )
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="kspies", # Replace with your own username
-    version="0.0.1",
+    version="1.0.0",
     author="Seungsoo Nam,  Ryan J. McCarty, Hansol Park, Eunji Sim",
     author_email="skaclitz@yonsei.ac.kr",
     description="This is a python based Kohn-Sham Inversion Evaluation Software package for use with pySCF.",
@@ -13,6 +24,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://ssnam92.github.io/KSPies/",
     packages=setuptools.find_packages(),
+    ext_modules = [lib],
+    install_requires=[
+        "numpy==1.18.4",
+        "scipy==1.4.1",
+        "pyscf==1.6.6",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
